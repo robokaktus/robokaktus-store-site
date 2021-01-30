e273996e7f2861a70432d735e6d2b5240d34b088
Макс <mr.bmv2018@ukr.net>
2021-01-31 01:19:59 +0300

    Установил Evolution Cms

---
+++ core/factory/version.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'version' => '3.0', // Current version number
+    'release_date' => 'Jan 23, 2021', // Date of release
+    'branch' => 'Evolution CMS', // Codebase name
+    'full_appname' => 'Evolution CMS 3.0 (Jan 23, 2021)'
+];
